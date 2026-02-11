82ce178f33e5e4517c3d064ca7cce084933e2a7d
fsdtmr <166978900+fsdtmr@users.noreply.github.com>
2026-02-11 02:50:04 +0000

    fix build

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lyrium/controller.dart';
 import 'package:lyrium/service/service.dart';
 import 'package:lyrium/widgets/settings.dart';
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -32,32 +33,19 @@ class AppDrawer extends StatelessWidget {
               applicationVersion: packageInfo.version,
               children: [
                 isWebsite
-                    ? TextButton.icon(
-                        onPressed: () {
-                          launchUrl(
-                            Uri.parse(
-                              "https://github.com/fsdtmr/lyrium/releases/latest",
-                            ),
-                          );
-                        },
+                    ? CopyValueButton(
+                        value:
+                            "https://github.com/fsdtmr/lyrium/releases/latest",
                         label: Text("Latest Release"),
                         icon: Icon(Icons.file_open),
                       )
-                    : TextButton.icon(
-                        onPressed: () {
-                          launchUrl(
-                            Uri.parse(
-                              "https://lyrium-lyrics.github.io/index.html",
-                            ),
-                          );
-                        },
+                    : CopyValueButton(
+                        value: "https://lyrium-lyrics.github.io/index.html",
                         label: Text("Web Version"),
                         icon: Icon(Icons.web),
                       ),
-                TextButton.icon(
-                  onPressed: () {
-                    launchUrl(Uri.parse("https://github.com/fsdtmr/lyrium"));
-                  },
+                CopyValueButton(
+                  value: "https://github.com/fsdtmr/lyrium",
                   label: Text("Github"),
                   icon: Icon(Icons.storage),
                 ),
@@ -82,5 +70,31 @@ class AppDrawer extends StatelessWidget {
     //   D
     //   XFile(path)
     // ]));
+  }
+}
+
+class CopyValueButton extends StatelessWidget {
+  final Widget label;
+  final String value;
+  final Widget icon;
+  const CopyValueButton({
+    super.key,
+    required this.value,
+    required this.label,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: () {
+        Clipboard.setData(ClipboardData(text: value));
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Copied to Clipboard")));
+      },
+      label: label,
+      icon: icon,
+    );
   }
 }
